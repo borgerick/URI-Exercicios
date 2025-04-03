fd9c6fd8a7b99803e6c491b964ae9604d79f8450
Erick Borges Rodrigues <borges.ericks@gmail.com>
2025-04-03 20:35:03 -0300

    ok

---
+++ URI1008/URI1008/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+int NumFuncionarios = int.Parse(Console.ReadLine());
+int NumHoras = int.Parse(Console.ReadLine());
+double Hora = double.Parse(Console.ReadLine());
+
+double Salario = NumHoras * Hora;
+
+Console.WriteLine("NUMBER = " + NumFuncionarios);
+Console.WriteLine("SALARY = U$ " + Salario.ToString("F2", System.Globalization.CultureInfo.InvariantCulture));
