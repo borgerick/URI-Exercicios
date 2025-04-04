d0dc77fefec9bba4abdbb88a4fef01be93c309da
Erick Borges Rodrigues <borges.ericks@gmail.com>
2025-04-03 21:20:11 -0300

    ok

---
+++ URI1010/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] dadosPeca1 = Console.ReadLine().Split(' ');
+int CodPeca1 = int.Parse(dadosPeca1[0]);
+int NumPeca1 = int.Parse(dadosPeca1[1]);
+double ValPeca1 = double.Parse(dadosPeca1[2], System.Globalization.CultureInfo.InvariantCulture);
+
+string[] dadosPeca2 = Console.ReadLine().Split(' ');
+int CodPeca2 = int.Parse(dadosPeca2[0]);
+int NumPeca2 = int.Parse(dadosPeca2[1]);
+double ValPeca2 = double.Parse(dadosPeca2[2], System.Globalization.CultureInfo.InvariantCulture);
+
+double total = (NumPeca1 * ValPeca1) + (NumPeca2 * ValPeca2);
+
+Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture));
---
+++ URI1014/Program.cs
@@ -0,0 +1,7 @@
+﻿
+int X = int.Parse(Console.ReadLine());
+double Y = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+double media = X / Y;
+
+Console.WriteLine(media.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + " km/l");
